004247c941283ef81a54470ddb1096fd40cb13f2
kabrow3 <kabrow3@gmail.com>
2018-06-25 17:38:07 -0400

    re-commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Giphy-API
+
+This is the Giphy-API Project
+
+It was completed for the Sixth Homework Assignment for the Coding Boot Camp
+
+It displays a series of buttons with the names of Superheroes as well as a text input box to take in the name of any hero the user wishes to enter.
+
+If one of the buttons is clicked, a sequence of 10 .gifs of the hero will display below the buttons and input box. Each .gif will dispolay in a still image state and only begin looping once it has been clicked. It will return to a static image state if it is clicked once more.
+
+If the user types into the input box and clicks the "add a hero" button, a new button with a name taken from the user input will be displayed. It will function identically to the previous buttons. This button adding feature can be completed an infinite number of times.
---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+var topics = ["Batman", "Captain America", "Iron Man", "Superman", "Spider-Man", "Wonder Woman"];
+
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var hero = $("<button>");
+      hero.addClass("hero");
+      hero.attr("data-name", topics[i]);
+      hero.text(topics[i]);
+      $("#buttons-view").append(hero);
+    }
+}
+
+function addButton() {
+    event.preventDefault();
+    var hero = $("#hero-input").val().trim();
+    topics.push(hero);
+    renderButtons();
+}
+
+
+    
+function displayGifs() {
+   var hero = $(this).attr("data-name");
+   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+   hero + "&api_key=witzkK7lz0aQdpPiDOatUym4TQQkj6qO&limit=10";
+   $.ajax({
+    url: queryURL,
+    method: "GET"
+   })
+    .then(function(response) {
+      var results = response.data;
+      console.log(response);
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var heroImage = $("<img>");
+        heroImage.attr("src", results[i].images.fixed_height_still.url);
+        heroImage.attr("data-still", results[i].images.fixed_height_still.url);
+        heroImage.attr("data-animate", results[i].images.fixed_height.url);
+        heroImage.attr("data-state", "still");
+        heroImage.addClass("gif");
+        gifDiv.append(p);
+        gifDiv.append(heroImage);
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
+}
+
+function changeState() {
+  var state = $(this).attr("data-state");
+  console.log(state);
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+}
+
+
+$(document).on("click", ".hero", displayGifs);
+$(document).on("click", "#add-hero", addButton);
+$(document).on("click", ".gif", changeState);
+
+renderButtons();
